730d5c21363c9131ee352bd340676c8a036eb554
Vincent Wang <376787823@qq.com>
2018-04-01 02:01:36 -0500

    Add files via upload

---
+++ lottie/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using LottieUWP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace lottie
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            animationView.Progress = 0.09f;
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            animationView.Progress = (float)(sender as Slider).Value;
+        }
+
+        private void animationView_ValueChanged(object sender, EventArgs e)
+        {
+            if (animationView.Progress >= 0.99f)
+                animationView.Progress = 0.18f;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame root = Window.Current.Content as Frame;
+            root.Navigate(typeof(BlankPage));
+        }
+
+    }
+}
